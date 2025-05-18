090f70dfa76a2a2c59059a389f5598b5c6c5528b
chuu-n <122401518+chuu-n@users.noreply.github.com>
2025-05-18 02:45:16 +0000

    Deploying to gh-pages from @ chuu-n/chuu-n.github.io@d6b185d419a325dddc4ccd1c606912594a874fc8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-blog",
-          title: "blog",
+        },{id: "nav-myanmar",
+          title: "Myanmar",
           description: "",
           section: "Navigation",
           handler: () => {
