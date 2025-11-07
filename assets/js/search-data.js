46e7cd56e371ae5bad83a385d6639d80413d046f
chuu-n <122401518+chuu-n@users.noreply.github.com>
2025-11-07 19:22:59 +0000

    Deploying to gh-pages from @ chuu-n/chuu-n.github.io@5f091893efcdece828c03f98609ce64a556e32ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-myanmar",
-          title: "Myanmar",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Myanmar/index.html";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
